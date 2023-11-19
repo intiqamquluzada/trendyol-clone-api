86851d8cb20bb4900d7c3404756dfeb4d38d245c
intiqamquluzada <quluzadintiqam@gmail.com>
2023-11-19 23:37:42 +0400

    smtp

--- core/settings.py
+++ core/settings.py
@@ -182,5 +182,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "quluzadintiqam@gmail.com"
-EMAIL_HOST_PASSWORD = 'pfhvqidrqptkrfts'
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ''
